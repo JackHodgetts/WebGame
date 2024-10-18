7933ae3d2493d176bc55348961f36fe8c2893253
JackHodgetts3 <Jack.Hodgetts@mail.bcu.ac.uk>
2024-10-18 14:18:29 +0100

    Event Example
    - Demo of the event listener using java

--- Web_/Javascript/script.js
+++ Web_/Javascript/script.js
@@ -1,1 +1,20 @@
-console.log("This is cool:)")
+console.log("This is cool:)");
+
+
+    
+    const clickFunction=()=>{
+    document.getElementById("demo").innerHTML = "This is new content";
+    }
+
+    
+    //This is demo of event listener
+    document.getElementById("demo").addEventListener("click", clickFunction);
+
+    /*
+    Function name: clickFunction
+    Author: Jack
+    Date: 18/10/24
+    */
+   
+
+
