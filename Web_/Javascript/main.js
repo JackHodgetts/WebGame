82ee9280842ad9fc18aeefe1ffcef274762f6d7a
JackHodgetts3 <Jack.Hodgetts@mail.bcu.ac.uk>
2024-10-18 16:29:29 +0100

    update
    - Window resizes

--- Web_/Javascript/main.js
+++ Web_/Javascript/main.js
@@ -8,18 +8,33 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 5, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0xedf50a } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const geometry = new THREE.BoxGeometry( 5, 1, 1 ); // makes geometry
+const material = new THREE.MeshBasicMaterial( { color: 0xedf50a } ); // makes the material
+const cube = new THREE.Mesh( geometry, material ); // makes the mesh for cube 1
+const cube2 = new THREE.Mesh( geometry, material ); // makes the mesh for cube 2
+scene.add( cube ); // adds cube to the scene
+scene.add( cube2); // add cube to the scene
 
-camera.position.z = 5;
+camera.position.z = 10; // camera position
+cube2.position.y = 2;
 
 function animate() {
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.01;
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 1;
 
 	renderer.render( scene, camera );
 
 }
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerWidth;
+    camera.aspect = window.innerHeight / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+window.addEventListener( "resize", onWindowResize );
