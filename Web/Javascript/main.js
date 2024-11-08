fbe4e8a3b99528f6f41f18794591813784072481
JackHodgetts3 <Jack.Hodgetts@mail.bcu.ac.uk>
2024-11-08 15:02:14 +0000

    Update

--- Web/Javascript/main.js
+++ Web/Javascript/main.js
@@ -46,9 +46,21 @@ const geometry = new THREE.BoxGeometry( 1, 1, 1 ); // makes geometry
 const material = new THREE.MeshBasicMaterial( { color: 0xedf50a } );
 const material2 = new THREE.MeshBasicMaterial( { color: 0xf2304a } ); // makes the material
 
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 5 );
-// light.position.set( 0, 1, 0 );
-scene.add( directionalLight );
+
+//Lights
+const createLights = ()=>{
+    const ambientLight = new THREE.HemisphereLight(0xddeeff, 0x202020, 0.8);
+
+    const mainLight = new THREE.DirectionalLight(0xffffff, 1);
+
+    mainLight.position.set(10, 10, 10);
+    scene.add( ambientLight, mainLight);
+}
+
+createLights();
+// const directionalLight = new THREE.DirectionalLight( 0xffffff, 5 );
+// // light.position.set( 0, 1, 0 );
+// scene.add( directionalLight );
 
 const cube = new THREE.Mesh( geometry, material ); // makes the mesh for cube 1
 const cube2 = new THREE.Mesh( geometry, material2 );
@@ -72,7 +84,26 @@ group2.add(group);
 
 scene.add(group2);
 
-//const geometry = 
+const geometry5 = new THREE.BoxGeometry(20, 20, 20);
+
+for (let i = 0; i < 2000; i ++){
+
+    const object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial ({ color: Math.random() * 0xffffff}));
+
+    object.position.x = Math.random() * 800 - 400;
+    object.position.y = Math.random() * 800 - 400;
+    object.position.z = Math.random() * 800 - 400;
+
+    object.rotation.x = Math.random() * 2 * Math.PI;
+    object.rotation.y = Math.random() * 2 * Math.PI;
+    object.rotation.z = Math.random() * 2 * Math.PI;
+
+    object.scale.x = Math.random() + 5;
+    object.scale.y = Math.random() + 5;
+    object.scale.z = Math.random() + 5;
+    
+    scene.add(object);
+}
 
 
 //function geneerate mesh
