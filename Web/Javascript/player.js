5f31ffdb2724694ba23118c69488bd64b8eb1ed5
JackHodgetts3 <Jack.Hodgetts@mail.bcu.ac.uk>
2024-11-13 18:46:21 +0000

    camera
    move camera to start of maze in the test html

--- Web/Javascript/player.js
+++ Web/Javascript/player.js
@@ -23,7 +23,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-camera.position.z = -10;
+camera.position.z = 30;
 camera.position.y = 2;
 
 //Lights
